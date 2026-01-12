3f1c16a556688799f0d624ad5d33d511916cf210
Gustavo Luiz Gregorio <gustavo.l.gregorio@gmail.com>
2026-01-12 02:52:51 -0300

    add: brio asset manager
    Object that stores assets, serving as a centralized asset object

---
+++ src/game/BrioAssetManager.ts
@@ -0,0 +1,50 @@
+import { BrioAudio } from "../assets/BrioAudio";
+import { BrioSprite } from "../assets/BrioSprite";
+import { BrioSpriteSheet } from "../assets/BrioSpriteSheet";
+import { BrioCamera } from "../not-implemented/BrioCamera";
+import { BrioMap } from "../not-implemented/BrioMap";
+import { BrioScene } from "../not-implemented/BrioScene";
+import { BrioObject } from "../objects/BrioObject";
+
+interface BrioAssets {
+    objects: Map<string, BrioObject>;
+    sprites: Map<string, BrioSprite>;
+    audios: Map<string, BrioAudio>;
+    maps: Map<string, BrioMap>;
+    cameras: Map<string, BrioCamera>;
+    scenes: Map<string, BrioScene>;
+    spritesheets: Map<string, BrioSpriteSheet>;
+}
+
+export class BrioAssetManager implements BrioAssets {
+    #objects = new Map<string, BrioObject>();
+    #sprites = new Map<string, BrioSprite>();
+    #audios = new Map<string, BrioAudio>();
+
+    #maps = new Map<string, BrioMap>();
+    #cameras = new Map<string, BrioCamera>();
+    #scenes = new Map<string, BrioScene>();
+    #spritesheet = new Map<string, BrioSpriteSheet>();
+
+    public get objects() {
+        return this.#objects;
+    }
+    public get sprites() {
+        return this.#sprites;
+    }
+    public get audios() {
+        return this.#audios;
+    }
+    public get maps() {
+        return this.#maps;
+    }
+    public get cameras() {
+        return this.#cameras;
+    }
+    public get scenes() {
+        return this.#scenes;
+    }
+    public get spritesheets() {
+        return this.#spritesheet;
+    }
+}
